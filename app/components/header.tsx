c3416e77e1b78dcc055f5ff0565c5cc0cef60495
Joshua Chou <joshuachou912@gmail.com>
2025-02-15 15:16:03 -0500

    added filter

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,30 +5,31 @@ import ThemeToggle from "./themetoggle";
 export default function Header() {
   return (
     <header>
-      <nav className="h-12 flex items-center justify-between px-6 bg-[#1a1f3d] shadow">
-        <Link href="/">
-          <div className="flex items-center gap-2">
-            <img
-              src="/images/logo.png"
-              alt="UofT Crest"
-              className="w-16 h-16"
-            />
-            <span className="text-base font-bold text-white">Discover UofT</span>
-          </div>
-        </Link>
-        <div className="flex items-center gap-4">
-          <Link href="/create" className="hover:underline text-white">
-            Create Event
-          </Link>
-          <Link href="/dashboard" className="hover:underline text-white">
-            Dashboard
-          </Link>
-          <Link href="/login" className="hover:underline text-white">
-            Login
-          </Link>
-          <ThemeToggle />
-        </div>
-      </nav>
+     <nav className="flex flex-wrap items-center justify-between px-6 bg-[#1a1f3d] shadow">
+  <Link href="/">
+    <div className="flex items-center gap-2">
+      <img
+        src="/images/logo.png"
+        alt="UofT Crest"
+        className="w-16 h-16"
+      />
+      <span className="text-base font-bold text-white">Discover UofT</span>
+    </div>
+  </Link>
+  <div className="flex items-center gap-4">
+    <Link href="/create" className="hover:underline text-white">
+      Create Event
+    </Link>
+    <Link href="/dashboard" className="hover:underline text-white">
+      Dashboard
+    </Link>
+    <Link href="/login" className="hover:underline text-white">
+      Login
+    </Link>
+    <ThemeToggle />
+  </div>
+</nav>
+
     </header>
   );
 }
