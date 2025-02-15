e8958167f5275b25e93dce2e6c369d6ba07c4531
corgi-in-tights <59304120+corgi-in-tights@users.noreply.github.com>
2025-02-15 14:51:47 -0500

    basic DAL

---
+++ app/database/dal/post.ts
@@ -0,0 +1,31 @@
+import { EventPost, EventPostInput, EventPostOutput } from '../models/post'
+
+export const create = async (payload: EventPostInput): Promise<EventPostOutput> => {
+    const post = await EventPost.create(payload)
+    return post
+}
+
+export const getById = async (id: number): Promise<EventPostOutput> => {
+    const post = await EventPost.findByPk(id)
+    if (!post) {
+        throw new Error("Failed to find EventPost with id: " + id)
+    }
+    return post
+}
+
+export const update = async (id: number, payload: Partial<EventPostInput>): Promise<EventPostOutput> => {
+    const post = await getById(id)
+    const updatedPost = await (post as EventPost).update(payload)
+    return updatedPost 
+}
+
+export const deleteById = async (id: number): Promise<boolean> => {
+    const deletedPostCount = await EventPost.destroy({
+        where: { id }
+    })
+    return !!deletedPostCount
+}
+
+export const getAll = async (): Promise<EventPostOutput[]> => {
+    return EventPost.findAll()
+}
